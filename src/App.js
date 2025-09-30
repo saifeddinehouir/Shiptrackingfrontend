25bfd1e6705cfb495508d31ea9fa4006be112a18
Saif-eddine Houir <saifeddinehouir01@gmail.com>
2025-09-30 21:09:04 +0000

    connected sockets

--- src/App.js
+++ src/App.js
@@ -8,8 +8,22 @@ import PortsPage from './pages/PortPage'; // Import the PortsPage component
 import './App.css';
 import SearchBar from './components/SearchBar';
 import VesselDetails from './components/VesselDetail'
+import io from 'socket.io-client';
 
 function App() {
+  const [aisData, setAisData] = React.useState('fetching...');
+  
+  React.useEffect(() => {
+    const socket =  io('http://localhost:5000');
+    socket.on('connect',()=>console.log(socket.id));
+    socket.on('connect_error', () => {
+      setTimeout(() => socket.connect(), 5000);
+    });
+    socket.on('aisData', (data) => setAisData(data));
+    socket.on('disconnect',()=>setAisData("disconnected"));
+    return () => socket.close();
+  }, []);
+  console.log(aisData);
   return (
     <div className="App">
         <SearchBar /> {/* Add the SearchBar here */}
